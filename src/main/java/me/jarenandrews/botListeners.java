b6ad0750d1e13faa4180116644303cf3a81bdf08
Jaren <4legojman12@gmail.com>
2023-02-25 03:54:28 -0800

    added some comments

--- src/main/java/me/jarenandrews/botListeners.java
+++ src/main/java/me/jarenandrews/botListeners.java
@@ -26,8 +26,8 @@ public class botListeners extends ListenerAdapter {
                     if(att.getSize() > 10485760) {
                         return;
                     }
-                    File temp = new File("filetosend.png");
-                    att.downloadToFile(temp).get();
+                    File temp = new File("draw.png");
+                    att.downloadToFile(temp).get(); //dont know current way, oh well
                     BufferedImage img = ImageIO.read(temp);
                     Graphics2D g = img.createGraphics();
                     //draw block
@@ -38,13 +38,13 @@ public class botListeners extends ListenerAdapter {
                     ImageIO.write(img, "png", temp);
                     event.getChannel().sendFiles(FileUpload.fromData(temp)).queue();
                     att.close(); //not sure that this helps
-                    if(!temp.delete()) {
+                    if(!temp.delete()) { //cant always delete?
                         System.out.println("Unable to delete " + temp.getName());
                     }
                 } catch (IOException | ExecutionException | InterruptedException e) {
                     e.printStackTrace();
                 }
-            } else {
+            } else { //echos message
                 String message = event.getMessage().getContentRaw();
                 event.getChannel().sendMessage("This was sent: " + message).queue();
             }
